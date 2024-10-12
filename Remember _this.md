f110b329c08c66b53c6d4b99c682b201c95469b3
VibhavKrishnanKS <ksvibhav@gmail.com>
2024-10-12 13:30:46 +0530

    My commit for section2 and section 3 for thius Junit Mockito Course

--- Remember _this.md
+++
@@ -1,7 +0,0 @@
-#### Comeback to this 
-
-1. Once when you learn Junit basics practically, come back to benefits of Junit and learn how each advantage works with a practical example   
-    1. Benefts
-    2. Integration Testing 
-    3. Popular Unit Testing framework
-
--- Section 2/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ Section 2/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -8,52 +8,56 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 //@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 public class DemoUtilsTest {
     DemoUtils demoUtils;
 
     @BeforeEach
-    void setupBeforeEach() {
+    void setupBeforeEach(){
         demoUtils = new DemoUtils();
     }
 
-    // @AfterEach
-    // void tearDownAfterEach(){
-    // System.out.println("Running @AfterEach");
-    // }
-    //
-    // // Also remember by default the methods must be static
-    // @BeforeAll
-    // static void setupBeforeEachClass(){
-    // System.out.println("@BeforeAll executes only once before all test methods
-    // execution in the class");
-    // }
-    //
-    // // Also remember by default the methods must be static
-    // @AfterAll
-    // static void tear(){
-    // System.out.println("@AfterAll executes only once after all test methods
-    // execution in the class");
-    // }
+    //    @AfterEach
+//    void tearDownAfterEach(){
+//        System.out.println("Running @AfterEach");
+//    }
+//
+//    // Also remember by default the methods must be static
+//    @BeforeAll
+//    static void setupBeforeEachClass(){
+//        System.out.println("@BeforeAll executes only once before all test methods execution in the class");
+//    }
+//
+//    // Also remember by default the methods must be static
+//    @AfterAll
+//    static void tear(){
+//        System.out.println("@AfterAll executes only once after all test methods execution in the class");
+//    }
+
+    @Test
+    @DisplayName("Multiply")
+    void testMultiply(){
+        assertEquals(12, demoUtils.multiply(3,4), "3 * 4 must be 12");
+    }
+
 
     // Testing the add method
     @Test
     @DisplayName("Equals and Not Equals")
-    void test_Equals_And_Not_Equals() {
+    void test_Equals_And_Not_Equals(){
 
-        // The test below will pass, if so the two values are equal. Else it will
-        // display the desired message
-        assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
-        //// The test below will pass, if so the two values are not equal. Else it will
-        //// display the desired message
-        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
+        // The test below will pass, if so the two values are equal. Else it will display the desired message
+        assertEquals(6,demoUtils.add(2,4), "2+4 must be 6");
+        //// The test below will pass, if so the two values are not equal. Else it will display the desired message
+        assertNotEquals(6,demoUtils.add(1,9), "1 + 9 must not be 6");
 
     }
 
     @Test
     @DisplayName("NULL and NOT NULL")
-    void test_Null_And_Not_Null() {
+    void test_Null_And_Not_Null(){
 
-        String str1 = null;
+        String str1 =null;
         String str2 = "Vibhav";
 
         assertNull(demoUtils.checkNull(str1), "Object should be null");
@@ -62,20 +66,17 @@ public class DemoUtilsTest {
 
     @Test
     @DisplayName("Same and Not Same")
-    void testSameAndNotSame() {
+    void testSameAndNotSame(){
 
-        // The test below will pass, if so the two values are same. Else it will display
-        // the desired message
+        // The test below will pass, if so the two values are same. Else it will display the desired message
         assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to the same object");
-        // The test below will pass, if so the two values are not same. Else it will
-        // display the desired message
-        assertNotSame(demoUtils.getAcademy(), demoUtils.getAcademyInList(),
-                "Objects should not refer to the same object");
+        // The test below will pass, if so the two values are not same. Else it will display the desired message
+        assertNotSame(demoUtils.getAcademy(), demoUtils.getAcademyInList(), "Objects should not refer to the same object");
     }
 
     @DisplayName("True and False")
     @Test
-    void testTrueFalse() {
+    void testTrueFalse(){
         int gradeOne = 10;
         int gradeTwo = 5;
 
@@ -85,45 +86,38 @@ public class DemoUtilsTest {
 
     @DisplayName("Array Equals")
     @Test
-    void testArrayEquals() {
-        String[] expectedArray = { "A", "B", "C" };
+    void testArrayEquals(){
+        String[] expectedArray = {"A", "B", "C"};
         String[] resultArray = demoUtils.getFirstThreeLettersOfAlphabet();
         assertArrayEquals(expectedArray, resultArray, "Arrays should be the same");
     }
 
     @DisplayName("Iterable Equals")
     @Test
-    void testIterableEquals() {
-        List<String> theList = List.of("luv", "2", "code");
+    void testIterableEquals(){
+        List<String> theList = List.of("luv", "2" , "code");
         assertIterableEquals(theList, demoUtils.getAcademyInList(), "Expected list should be same as actual list");
     }
 
     @DisplayName("Lines match")
     @Test
-    void testLinesMatch() {
-        List<String> theList = List.of("luv", "2", "code");
+    void testLinesMatch(){
+        List<String> theList = List.of("luv","2","code");
         assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines Should match");
     }
 
     @DisplayName("Throws and Does Not Throw")
     @Test
-    void testThrowAndDoesNotThrow() {
-        assertThrows(Exception.class, () -> {
-            demoUtils.throwException(-1);
-        }, "Should throw exception");
-        // As the function accepts a value that is greater than zero it shouldn't throw
-        // any exception
-        assertDoesNotThrow(() -> {
-            demoUtils.throwException(9);
-        }, "Should not throw exception");
+    void testThrowAndDoesNotThrow(){
+        assertThrows(Exception.class, () -> { demoUtils.throwException(-1);}, "Should throw exception");
+        // As the function accepts a value that is greater than zero it shouldn't throw any exception
+        assertDoesNotThrow(() -> { demoUtils.throwException(9);}, "Should not throw exception");
     }
 
     @DisplayName("Timeout")
     @Test
-    void testTimeout() {
-        assertTimeoutPreemptively(Duration.ofSeconds(2), () -> {
-            demoUtils.checkTimeout();
-        }, "Method should execute in 2 seconds");
+    void testTimeout(){
+        assertTimeoutPreemptively(Duration.ofSeconds(3), ()->{demoUtils.checkTimeout();}, "Method should execute in 3 seconds");
     }
 
 }
